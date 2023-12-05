5e862b3bf35e43ba3c82019805eb64c58ffb43b7
Krishna <singhks0054@gmail.com>
2023-12-05 16:03:58 +0530

    Update package.json and mojo/page.tsx

--- app/mojo/page.tsx
+++ app/mojo/page.tsx
@@ -1,34 +1,32 @@
-"use client"
-import { gsap } from 'gsap'
-import { useLayoutEffect, useRef, useState } from 'react'
-import { ScrollTrigger, TextPlugin } from 'gsap/all'
+'use client';
+import { gsap } from 'gsap';
+import { useLayoutEffect, useRef, useState } from 'react';
+import { ScrollTrigger, TextPlugin } from 'gsap/all';
 
 export default function Coucher() {
-  const [render, setRender] = useState(0)
-  const tl = gsap.timeline()
+  const [render, setRender] = useState(0);
+  const tl = gsap.timeline();
 
-  gsap.registerPlugin(TextPlugin)
-  gsap.registerPlugin(ScrollTrigger)
+  gsap.registerPlugin(TextPlugin);
+  gsap.registerPlugin(ScrollTrigger);
 
-  // function time() {
-  //   let a = 0
-  //   setInterval(function () {
-  //     a = a + Math.floor(Math.random() * 10)
+  function time() {
+    let a = 0;
+    setInterval(function () {
+      a = a + Math.floor(Math.random() * 10);
 
-  //     if (a < 100) {
-  //       // @ts-ignore
-  //       document.querySelector('#loader h1').innerHTML = a + '%'
-  //     } else {
-  //       a = 100
-  //       // @ts-ignore
-  //       document.querySelector('#loader h1').innerHTML = a + '%'
-  //     }
+      if (a < 100) {
+        // @ts-ignore
+        document.querySelector('#loader h1').innerHTML = a + '%';
+      } else {
+        a = 100;
+        // @ts-ignore
+        document.querySelector('#loader h1').innerHTML = a + '%';
+      }
 
-  //     console.log(a)
-  //   }, 150)
-
-  // }
-
+      console.log(a);
+    }, 150);
+  }
 
   useLayoutEffect(() => {
     // tl.to("#loader h1",
@@ -45,31 +43,34 @@ export default function Coucher() {
     // })
     tl.to('#home h1', {
       transform: 'translateX(-100%)',
-      fontWeight: "100",
+      fontWeight: '100',
       scrollTrigger: {
-        trigger: "#home",
-        scroller: "body",
+        trigger: '#home',
+        scroller: 'body',
         markers: true,
-        start: "top 0",
-        end: "top -100%",
+        start: 'top 0',
+        end: 'top -100%',
         scrub: 3,
-        pin: true
-      }
-    })
-  }, [render])
-
-
+        pin: true,
+      },
+    });
+  }, [render]);
 
   return (
     <>
-      {/* <div id='loader' className="h-screen bg-slate-400 font-sans font-semibold flex justify-center items-center" >
-        <h1 className="text-4xl" id="counter">
+      <div
+        id='loader'
+        className='h-screen bg-slate-400 font-sans font-semibold flex justify-center items-center'
+      >
+        <h1 className='text-4xl' id='counter'>
           0%
         </h1>
-      </div> */}
+      </div>
       <div className='h-screen w-full  flex items-center' id='home'>
-        <h1 className="text-[500px] whitespace-nowrap extrabold  ">WE BUILD BRANDS!</h1>
+        <h1 className='text-[500px] whitespace-nowrap extrabold  '>
+          WE BUILD BRANDS!
+        </h1>
       </div>
     </>
-  )
+  );
 }
